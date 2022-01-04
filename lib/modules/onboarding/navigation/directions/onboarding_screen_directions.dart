cf8bd9a12fde40a2c784e19223bc22c0f953fc80
RenanLopesAlmeida <renan.lopes.contato@gmail.com>
2022-01-04 15:34:48 -0300

    stricting the screen in parameter

--- lib/modules/onboarding/navigation/directions/onboarding_screen_directions.dart
+++ lib/modules/onboarding/navigation/directions/onboarding_screen_directions.dart
@@ -1,9 +1,12 @@
 import 'package:taplingo_matrix/core/directions/screen_directions.dart';
 import 'package:taplingo_matrix/core/routes/app_navigator.dart';
 import 'package:taplingo_matrix/modules/login/navigation/routes/login_route.dart';
+import 'package:taplingo_matrix/modules/onboarding/ui/onboarding_screen.dart';
 
 class OnboardingScreenDirections extends ScreenDirection {
-  OnboardingScreenDirections(screen) : super(screen);
+  OnboardingScreenDirections(
+    OnboardingScreen screen,
+  ) : super(screen);
 
   void goToLogin() {
     AppNavigator.pushReplacement(LoginRoute.of());
